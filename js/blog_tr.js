75d58f3197b5bccb304b1ae70aea933d20a9c445
arastokdemir1 <119787040+arastokdemir1@users.noreply.github.com>
2025-10-29 15:21:14 +0300

    Update and rename blog.js to blog_tr.js

--- js/blog_tr.js
+++ js/blog_tr.js
@@ -7,7 +7,7 @@ const blogPosts = [
         author: "Aras Tokdemir",
         date: "29 Ekim 2025",
         tag: "Tavsiye",
-        image: "img/tissot-prx-powermatic-80-t137407110410-495e-b.png", // Lütfen bu yolu kendi görselinizle güncelleyin
+        image: "img/tissot/tissot-prx-powermatic-80-t137407110410-495e-b.png", // Lütfen bu yolu kendi görselinizle güncelleyin
         techMin: "PRX'in 80 saatlik güç rezervi harika, kasa formu ise 70'lerin ruhunu modern bir rahatlıkla taşıyor. Ancak bu saat sadece teknik bir kağıt parçası değil...",
         subjective: "Bana göre PRX, bir 'spor klasik' saatin zirvesidir. Takım elbiseyle gümüş bir yansıması, kot ceketle ise sofistike bir dokunuş katar. Spor saatinizi çıkarıp saklamak yerine, bir Cuma iş çıkışı kokteylinde veya rahat bir Pazar kahvaltısında bileğinizde tutmaya devam edebileceğiniz tek saat budur. Günlük yaşamda 'tek saat' arayan, lüks hissiyatı uygun bütçeyle isteyenler için tek tavsiyemdir.",
         link: "#" 
@@ -18,7 +18,7 @@ const blogPosts = [
         author: "WatchCompare Editör",
         date: "15 Ekim 2025",
         tag: "Nostalji",
-        image: "img/seiko/skx007k2.jpg", // Lütfen bu yolu kendi görselinizle güncelleyin
+        image: "img/seiko/seiko_skx_example.jpg", // Lütfen bu yolu kendi görselinizle güncelleyin
         techMin: "SKX'in güncel mekanizmalara göre biraz hantal kalması veya saniye durdurma özelliğinin olmaması önemli mi? Hayır, çünkü SKX bir saatten çok bir karaktere sahip...",
         subjective: "Bu saat, 'gerçek' bir maceranın kokusunu taşıyor. Her ne kadar artık üretilmese de, bileğinizde taşıdığınızda size bir ofis çalışanı değil de, okyanus kaşifi veya eski bir savaş pilotu hissi veriyor. Kordonunu değiştirerek sonsuz varyasyon yaratabilirsiniz; bu, saatin sizin kişisel tuvaliniz olması demektir. SKX bir trend değil, nesilden nesile aktarılacak bir hikayedir.",
         link: "#"
